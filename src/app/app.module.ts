9a6bcc438a9b7059890effa0b06026043c276b76
Jimmy Sené <jimmy.sene@laposte.net>
2018-02-14 16:15:31 +0100

    Première page de heLP

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { IdentiteComponent } from './identite/identite.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IdentiteComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
